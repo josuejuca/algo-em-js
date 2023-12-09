f653fbe6aef445058fd11a32fbab806b23c7ce8c
josuejuca <josuejuca19@gmail.com>
2023-12-09 12:30:23 -0300

    Alerts, Confirm e prompt

---
+++ alerts/main.js
@@ -0,0 +1,12 @@
+alert('Isso aqui e um alert (alert)');
+confirm('Isso é uma caixa que pede confirmação (confirm)'); // pode retornar um boolean
+prompt('Isso é uma caixa que pede informacao (prompt)');
+
+alert('Eu consigo trabalhar com essas caixinhas');
+
+const num1 = Number(prompt('Digite um novo número: '));
+const num2 = Number(prompt('Digite outro número: '));
+
+const soma = num1 + num2;
+
+alert(`Soma de ${num1} + ${num2} = ${soma}`);
