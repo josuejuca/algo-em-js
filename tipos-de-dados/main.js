9019c3fcff6e633e824b3abab9b303a17eb9c724
josuejuca <josuejuca19@gmail.com>
2023-12-09 11:22:20 -0300

    Tipos de dados

---
+++ tipos-de-dados/main.js
@@ -0,0 +1,11 @@
+const nome = 'Juca'; // String
+const nome1 = 'Juca';  // String
+const nome2 = 'Juca'; // String
+const nome3 = 'Juca'; // String
+const num1 = 10; // Number
+const num2 = 10.5; // Number
+let nomeAluno; // undefined -> não aponta para nenhum lugar na memoria 
+const sobrenomeAluno = null; // nulo -> não aponta para nenhum lugar na memoria 
+const aprovado = true;  // boolean = true, false (Logico)
+
+console.log( typeof aprovado, aprovado);
