ec7b779f2942dda8684f4897adb0f1787c6a8623
josuejuca <josuejuca19@gmail.com>
2023-12-10 06:59:49 -0300

    Switch Case

---
+++ switch-case/main.js
@@ -0,0 +1,51 @@
+const data = new Date();
+const diaSemana = data.getDay();
+let diaSemanaTexto;
+
+// Com IF e ELSE
+if (diaSemana === 0) {
+    diaSemanaTexto = `Domingo`;
+} else if (diaSemana === 1) {
+    diaSemanaTexto = `Segunda`;
+} else if (diaSemana === 2) {
+    diaSemanaTexto = `Terça`;
+} else if (diaSemana === 3) {
+    diaSemanaTexto = `Quarta`;
+} else if (diaSemana === 4) {
+    diaSemanaTexto = `Quinta`;
+} else if (diaSemana === 5) {
+    diaSemanaTexto = `Sexta`;
+} else if (diaSemana === 6) {
+    diaSemanaTexto = `Sábado`;
+} else {
+    diaSemanaTexto = ``;
+}
+// Com Switch e Case 
+switch (diaSemana) {
+    case 0:
+        diaSemanaTexto = `Domingo`;
+        break;
+    case 1:
+        diaSemanaTexto = `Segunda`;
+        break;
+    case 2:
+        diaSemanaTexto = `Terça`;
+        break;
+    case 3:
+        diaSemanaTexto = `Quarta`;
+        break;
+    case 4:
+        diaSemanaTexto = `Quinta`;
+        break;
+    case 5:
+        diaSemanaTexto = `Sexta`;
+        break;
+    case 6:
+        diaSemanaTexto = `Sábado`;
+        break;
+    default:
+        diaSemanaTexto = ``;
+        break;
+}
+
+console.log(diaSemana, diaSemanaTexto);
