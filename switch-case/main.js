754e81ed5dc4d1d857ed9c2abbd8c02aea80c380
josuejuca <josuejuca19@gmail.com>
2023-12-10 07:13:02 -0300

    Switch Case - Criando função getDiaSemanaTexto

--- switch-case/main.js
+++ switch-case/main.js
@@ -1,6 +1,6 @@
 const data = new Date();
 const diaSemana = data.getDay();
-let diaSemanaTexto;
+let diaSemanaTexto = getDiaSemanaTexto(diaSemana);
 
 // Com IF e ELSE
 if (diaSemana === 0) {
@@ -47,5 +47,36 @@ switch (diaSemana) {
         diaSemanaTexto = ``;
         break;
 }
+// Com função
+function getDiaSemanaTexto(diaSemana) {
+    let diaSemanaTexto;
+    switch (diaSemana) {
+        case 0:
+            diaSemanaTexto = `Domingo`;
+            return diaSemanaTexto;
+        case 1:
+            diaSemanaTexto = `Segunda`;
+            return diaSemanaTexto;
+        case 2:
+            diaSemanaTexto = `Terça`;
+            return diaSemanaTexto;
+        case 3:
+            diaSemanaTexto = `Quarta`;
+            return diaSemanaTexto;
+        case 4:
+            diaSemanaTexto = `Quinta`;
+            return diaSemanaTexto;
+        case 5:
+            diaSemanaTexto = `Sexta`;
+            return diaSemanaTexto;
+        case 6:
+            diaSemanaTexto = `Sábado`;
+            return diaSemanaTexto;
+        default:
+            diaSemanaTexto = ``;
+            return diaSemanaTexto;
+    }
+}
+
 
 console.log(diaSemana, diaSemanaTexto);
